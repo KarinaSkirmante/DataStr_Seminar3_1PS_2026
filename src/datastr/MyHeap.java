6457e017623a0e8520a798d3b257ad68a6c3e6e0
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-10 17:24:24 +0300

    Add: added MyHeap class variables, get, isEmpty and isFull

--- src/datastr/MyHeap.java
+++ src/datastr/MyHeap.java
@@ -1,5 +1,32 @@
 package datastr;
 
-public class MyHeap {
+public class MyHeap <Ttype> {
+	private MyNode<Ttype> rootNode = null;
+	private MyNode<Ttype> lastNode = null;
+	private int howManyElements = 0;
+	
+	public int getHowManyElements() {
+		return howManyElements;
+	}
+	
+	//get funkciajs netaisām blokiem, lai lietotajs tiem netiek klāt
+	//set funkcijas netaisām, jo neļaujam lieottajam mainīt ne blokus, ne arī elementa skaitu
+	
+	//bez argumenta konstruktors nāks no Object klases, tāpec paši netaisām
+	
+	public boolean isEmpty() {
+		return (howManyElements == 0);
+	}
+	
+	public boolean isFull() {
+		try {
+			new MyNode('a');
+			return false;
+		} catch (OutOfMemoryError error) {
+			return true;
+		}
 
+	}
+
+	
 }
