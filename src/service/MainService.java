91c7e0c4a412c873e777deec964c0c25399e62f1
KarinaSkirmante <karina.krinkele@gmail.com>
2026-04-10 18:57:49 +0300

    Add: added Mainservice with some testing ( not complete working)

---
+++ src/service/MainService.java
@@ -0,0 +1,23 @@
+package service;
+
+import datastr.MyHeap;
+
+public class MainService {
+
+	public static void main(String[] args) {
+		MyHeap<Integer> heapForInt = new MyHeap<Integer>();
+		
+		try
+		{
+			heapForInt.add(10);// P -> 10
+			heapForInt.add(20);// P -> 20, LC -> 10
+			heapForInt.add(30);// P -> 30, LC -> 10, RC -> 20
+			heapForInt.print();
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
